3c9634efaacff0e98365f0bc9f3f1f4a59046b08
Evgeniy <evgenij.k@serviceweb.su>
2021-01-21 16:25:41 +0300

    create table component

--- Desktop/crypto_conv/src/App.tsx
+++ Desktop/crypto_conv/src/App.tsx
@@ -1,30 +1,65 @@
-import { Container, Paper, Grid, TextField, Select, MenuItem } from '@material-ui/core';
-import React from 'react';
-import './App.css';
+import {
+  Container,
+  Paper,
+  Grid,
+  TextField,
+  Select,
+  MenuItem,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <Container maxWidth='lg'>
+    <Container maxWidth="lg">
       <Grid container spacing={3}>
         <Grid item xs={7}>
           <Paper elevation={2}>123</Paper>
         </Grid>
         <Grid item xs={5}>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            //value={1}
-            //onChange={1}
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
+          <Paper elevation={2}>
+            <div>
+              <TextField
+                id="outlined-basic"
+                label="Outlined"
+                variant="outlined"
+              />
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={10}
+                onChange={(e) => alert(e.target.value)}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </div>
+            <div>
+              <TextField
+                id="outlined-basic"
+                label="Outlined"
+                variant="outlined"
+              />
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={10}
+                onChange={(e) => alert(e.target.value)}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </div>
+            <Typography variant="h5" component="h3">
+              wwwwwwwwwwwww
+            </Typography>
+          </Paper>
         </Grid>
       </Grid>
     </Container>
-    
   );
 }
 
---
+++ Desktop/crypto_conv/src/components/CryptoTable/CryptoTable.tsx
@@ -0,0 +1,12 @@
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core'
+import React from 'react'
+
+function CryptoTable() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CryptoTable
