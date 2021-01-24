d23cd566fad6d0497b5f78a208a7f4caa2cffd6b
Eugene Koryabkin <eugenewalknut@gmail.com>
2021-01-24 20:26:12 +0300

    clear dirs

--- Desktop/docs/models/Favs.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model, Types} = require('mongoose')
-
-const schema = new Schema({
-    link: {type: String, required: true},
-    title: {type: String, required: true},
-    owner:{type: Types.ObjectId, ref: 'User'}
-})
-
-module.exports = model('Favs', schema)
